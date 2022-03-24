0c7fe16a4075382042f9abe08612415ec26ee193
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-24 17:51:30 -0600

    :iphone: Work Homeview Responsive design
    Work Homewview Responsive design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class=" mt-[10vh] h-[90vh] p-5 flex flex-col items-center justify-center space-y-5">
-    <div class="" >
-      <h1 class=" font-bold text-red-500 text-5xl text-center" >!La felicidad en forma de pizza¡</h1>
+  <div class=" mt-[10vh] h-[90vh] lg:mt-0 p-5 lg:p-0 flex flex-col lg:flex-row lg:h-[90vh] items-center justify-center space-y-5">
+    <div class=" lg:w-[50%] lg:h-full lg:flex lg:flex-col lg:justify-center lg:bg-red-500" >
+      <h1 class=" font-bold text-red-500 lg:text-white lg:text-7xl text-center" >!La felicidad en forma de pizza¡</h1>
+
     </div>
-    <img src="https://i.imgur.com/yOQJCss.png" alt="">
-    <router-link to="/menu" class=" w-[80%] text-3xl flex justify-center  bg-orange-400 px-4 py-2 rounded-full text-white font-bold shadow shadow-stone-500" >Ir al menu</router-link>
+    <img class=" lg:w-[50%]" src="https://i.imgur.com/yOQJCss.png" alt="">
+    <router-link to="/menu" class=" w-[50%] lg:absolute lg:bottom-5 text-3xl flex justify-center  bg-orange-400 px-4 py-2 rounded-full text-white font-bold shadow shadow-stone-500" >Ir al menu</router-link>
   </div>
 </template>
 
