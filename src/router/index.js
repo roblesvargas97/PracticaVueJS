fd9e6fb9acd468bf1a9f16111f7c7e6fdadb3920
roblesvargas97 <roblesvargas97@gmail.com>
2022-03-24 20:15:54 -0600

    :bug: Fix bug Router
    Fix Bug Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MenuView from '../views/MenuView.vue'
+import BuildYourPizzaView from '../views/BuildYourPizzaView.vue'
+import OrderView from '../views/OrderView.vue'
+import OwnerView from '../views/OwnerView.vue'
 
 const routes = [
   {
@@ -10,25 +14,29 @@ const routes = [
   {
     path: '/menu',
     name: 'menu',
+    component: MenuView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/MenuView.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/MenuView.vue')
   },
   {
     path: '/build-pizza',
     name: 'buildPizza',
-    component: () => import ('../views/BuildYourPizzaView.vue')
+    component: BuildYourPizzaView
+    // component: () => import ('../views/BuildYourPizzaView.vue')
   },
   {
     path: '/order-view',
     name: 'orderView',
-    component: () => import ('../views/OrderView.vue')
+    component: OrderView
+    // component: () => import ('../views/OrderView.vue')
   },
   {
     path: '/owner-view',
     name: 'ownerView',
-    component: () => import ('../views/OwnerView.vue')
+    component: OwnerView
+    // component: () => import ('../views/OwnerView.vue')
   }
 ]
 
